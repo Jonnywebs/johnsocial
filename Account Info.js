19afa644b959b11253d78e1fc1dbd4813f31c068
samuelolusamuel04 <131404473+samuelolusamuel04@users.noreply.github.com>
2023-08-05 00:52:44 +0100

    Add files via upload
    My Final Project

---
+++ Account Info.js
@@ -0,0 +1,17 @@
+const savedData1 = localStorage.getItem('firstData');
+                document.getElementById('firstName').value = savedData1;
+
+                const savedData2 = localStorage.getItem('secondData');
+                document.getElementById('lastName').value = savedData2;
+
+                const savedData3 = localStorage.getItem('thirdData');
+                document.getElementById('phoneNumber').value = savedData3;
+
+                const savedData4 = localStorage.getItem('fourthData');
+                document.getElementById('selectGender').value = savedData4;
+
+                const savedData5 = localStorage.getItem('fifthData');
+                document.getElementById('emailValue').value = savedData5;
+
+                const savedData6 = localStorage.getItem('sixthData');
+                document.getElementById('passwordValue').value = savedData6;
